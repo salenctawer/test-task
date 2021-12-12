c050b6e3b74f8dbdf1e8be1016751d77c15d1a5f
salenctawer <salenctawer@gmail.com>
2021-12-12 21:26:12 +0500

    login validation fixed

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -1,29 +1,79 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import styles from './Login.module.scss'
+import styles from "./Login.module.scss";
+import { useForm } from "react-hook-form";
 
-const Login = (props) =>{
-    const [login, setLogin] = useState('')
-    const [password, setPassword] = useState('')
-    const submitForm = (e) =>{
-        e.preventDefault()
-        if(props.users.find(user => user.login === login && user.password===password)){
-            alert('Вход успешен')
-        }
-        else {
-            alert('Неправильный логин или пароль')
-        }
+const Login = (props) => {
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm();
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+  const onSubmit = () => {
+    if (
+      props.users.find(
+        (user) => user.login === login && user.password === password
+      )
+    ) {
+      alert("Вход успешен");
+    } else {
+      alert("Неправильный логин или пароль");
     }
-    return(
-        <div className={styles.loginPage}>
-        <NavLink className={styles.loginPage__link} to='/'>Регистрация аккаунта</NavLink>
-            <form className={styles.form}>
-                <input className={styles.form__input} onChange={e => setLogin(e.target.value)} value={login} placeholder="Введите логин"></input>
-                <input className={styles.form__input} onChange={e => setPassword(e.target.value)} value={password} placeholder="Введите пароль"></input>
-                <button className={styles.form__button} type="submit" onClick={submitForm}>Войти</button>
-            </form>
-        </div>
-    )
-}
+    setValue("login", "");
+    setValue("password", "");
+  };
+  const onLoginChange = (e) =>{
+    setLogin(e.target.value)
+  }
+  const onPasswordChange = (e) =>{
+    setPassword(e.target.value)
+  }
+  return (
+    <div className={styles.loginPage}>
+      <NavLink className={styles.loginPage__link} to="/">
+        Регистрация аккаунта
+      </NavLink>
+      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+        <input
+          className={styles.form__input}
+          onChange={(e) => setLogin(e.target.value)}
+          name="login"
+          type="text"
+          placeholder="Введите логин"
+          {...register("login", {
+            onChange: onLoginChange,
+            required: {
+              value: true,
+              message: "Это поле обязательно",
+            },
+          })}
+        ></input>
+        <span className={styles.form__error}>
+            {errors.login && errors.login.message}
+        </span>
+        <input
+          className={styles.form__input}
+          placeholder="Введите пароль"
+          {...register("password", {
+            onChange: onPasswordChange,
+            required: {
+              value: true,
+              message: "Это поле обязательно",
+            },
+          })}
+        ></input>
+        <span className={styles.form__error}>
+            {errors.password && errors.password.message}
+        </span>
+        <button className={styles.form__button} type="submit">
+          Войти
+        </button>
+      </form>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
--- src/component/Main/Main.jsx
+++ src/component/Main/Main.jsx
@@ -1,73 +1,94 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import styles from './Main.module.scss'
+import styles from "./Main.module.scss";
 import { useForm } from "react-hook-form";
 
 const Main = (props) => {
-  const { register, handleSubmit, setValue, formState: { errors } } = useForm();
-  const onLoginChange = (e) =>{
-    props.updateLogin(e.target.value)
-  }
-  const onPasswordChange = (e) =>{
-    props.updatePassword(e.target.value)
-  }
-  const onSubmit = () =>{
-    props.updateId(props.id + 1)
-    props.addUser()
-    setValue('login', '')
-    setValue('password', '')
-  }
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm();
+  const onLoginChange = (e) => {
+    props.updateLogin(e.target.value);
+  };
+  const onPasswordChange = (e) => {
+    props.updatePassword(e.target.value);
+  };
+  const onSubmit = () => {
+    props.updateId(props.id + 1);
+    props.addUser();
+    setValue("login", "");
+    setValue("password", "");
+  };
 
   return (
     <div className={styles.main}>
-      <NavLink className={styles.main__link} to='/login'>Вход в аккаунт</NavLink >
+      <NavLink className={styles.main__link} to="/login">
+        Вход в аккаунт
+      </NavLink>
       <div className={styles.users}>
         <h2>Созданные пользователи</h2>
         <div className={styles.users__table}>
-        <div className={styles.users__column}>
-              <h3>Id</h3>
-              {props.users.map(user => <span>{user.id}</span>)}
+          <div className={styles.users__column}>
+            <h3>Id</h3>
+            {props.users.map((user) => (
+              <span>{user.id}</span>
+            ))}
           </div>
           <div className={styles.users__column}>
-              <h3>Логин</h3>
-              {props.users.map(user => <span>{user.login}</span>)}
+            <h3>Логин</h3>
+            {props.users.map((user) => (
+              <span>{user.login}</span>
+            ))}
           </div>
           <div className={styles.users__column}>
-              <h3>Пароль</h3>
-              {props.users.map(user => <span>{user.password}</span>)}
+            <h3>Пароль</h3>
+            {props.users.map((user) => (
+              <span>{user.password}</span>
+            ))}
           </div>
         </div>
       </div>
       <div className={styles.addUsers}>
         <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
           <h2>Форма создания пользователя</h2>
-          <input className={styles.form__input}
-          placeholder="Введите логин"
-          name='login'
-          type='text' 
-          {...register('login',{
-            onChange: onLoginChange,
-            required: {
-              value: true,
-              message: 'Это поле обязательно'
-            }
-          })}  
+          <input
+            className={styles.form__input}
+            placeholder="Введите логин"
+            name="login"
+            type="text"
+            {...register("login", {
+              onChange: onLoginChange,
+              required: {
+                value: true,
+                message: "Это поле обязательно",
+              },
+            })}
           />
-          <span className={styles.form__error}>{errors.login && errors.login.message}</span> 
-          <input className={styles.form__input}
-          placeholder="Введите логин"
-          name='password'
-          type='text' 
-          {...register('password',{
-            onChange: onPasswordChange,
-            required: {
-              value: true,
-              message: 'Это поле обязательно'
-            }
-          })}
+          <span className={styles.form__error}>
+            {errors.login && errors.login.message}
+          </span>
+          <input
+            className={styles.form__input}
+            placeholder="Введите пароль"
+            name="password"
+            type="text"
+            {...register("password", {
+              onChange: onPasswordChange,
+              required: {
+                value: true,
+                message: "Это поле обязательно",
+              },
+            })}
           />
-          <span className={styles.form__error}>{errors.password && errors.password.message}</span> 
-          <button className={styles.form__button} type='submit'>Зарегистрироваться</button>
+          <span className={styles.form__error}>
+            {errors.password && errors.password.message}
+          </span>
+          <button className={styles.form__button} type="submit">
+            Зарегистрироваться
+          </button>
         </form>
       </div>
     </div>
